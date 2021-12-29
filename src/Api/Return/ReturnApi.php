fab967e6e29be0796de30d0e9e6f62b2498653cb
Dayanne Garcia <48609156+DayGarcia@users.noreply.github.com>
2021-12-29 16:02:38 -0300

    [shopee] return and items

---
+++ src/Api/Return/ReturnApi.php
@@ -0,0 +1,49 @@
+<?php
+
+
+namespace LaravelShopee\Api\Return;
+
+use LaravelShopee\Api;
+use LaravelShopee\Configuration;
+
+class ReturnApi extends Api
+{
+
+    public function __construct(Configuration $configuration)
+    {
+        $this->configuration = $configuration;
+    }
+
+    public function getReturns(int $page_no, int $page_size, string $create_time_from = null, string $create_time_to = null): array
+    {
+        $query = [
+            'page_no' => $page_no,
+            'page_size' => $page_size,
+            'create_time_from' => $create_time_from,
+            'create_time_to' => $create_time_to,
+        ];
+        $path = 'returns/get_return_list';
+        return $this->get($this->configuration, $path, $query);
+    }
+
+    public function getReturnDetail(int $return_sn): array
+    {
+        $query = [
+            'return_sn' => $return_sn,
+        ];
+        $path = 'returns/get_return_detail';
+        return $this->get($this->configuration, $path, $query);
+    }
+
+    public function confirmReturn(array $data): array
+    {
+        $path = 'returns/confirm';
+        return $this->post($this->configuration, $path, $data);
+    }
+
+    public function disputeReturn(array $data): array
+    {
+        $path = 'returns/dispute';
+        return $this->post($this->configuration, $path, $data);
+    }
+}
