f82eec38f58ca534f4f4a1fec21fe2b2be206a86
Dayanne Garcia <48609156+DayGarcia@users.noreply.github.com>
2022-01-05 13:07:50 -0300

    added order cancel requestr

---
+++ src/App/Http/Requests/Order/PostCancelOrderRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\LaravelShopee\Order;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PostCancelOrderRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'cancel_reason' => 'OUT_OF_STOCK,CUSTOMER_REQUEST,UNDELIVERABLE_AREA,COD_NOT_SUPPORTED',
+            'item_list' => 'required|array',
+            'item_list.*.item_id'  => 'required|integer',
+            'item_list.*.model_id' => 'required|integer',
+        ];
+    }
+}
